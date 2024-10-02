a3751ac532579d8161922f6cccdc7700983e47f7
Rob Curtis <surreymagpie@yahoo.co.uk>
2024-10-02 14:17:27 +0100

    Add LSP configuration
    Formatting changes to other files after linting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })()
+  end,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "vim", "javascript", "html", "php", "css" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+    configs.setup({
+      ensure_installed = { "lua", "vim", "javascript", "html", "php", "css" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
-
